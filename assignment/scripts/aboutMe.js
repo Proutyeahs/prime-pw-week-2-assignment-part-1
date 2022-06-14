9c9dc1725380fccb34bdea42746f5941a9019f5a
Eric Prouty <Eric.W.Prouty@gmail.com>
2022-06-14 11:25:27 -0500

    Complete

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -19,9 +19,9 @@ let adventurous = true;
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
 let food = "Cheap Ramen";
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
-let pets = 7;
+let pets = 2;
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
-let friendsPets = 1;
+let friendsPets = 4;
 // 11 - Add two pets to your `pets` variable
 pets += 2;
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
@@ -58,7 +58,14 @@ if (pets < allowedPets) {
 // assigns the highest value to `mostPets`. There's several possibilities --
 // be sure to think through all the scenarios. 
 // console.log `mostPets` after the conditional has run.
-
+let mostPets;
+if (pets > friendsPets) {
+    mostPets = pets;
+} else if (pets < friendsPets) {
+    mostPets = friendsPets;
+} else {
+    mostPets = "Both my friend and I have the same amount of pets"
+};
 console.log(mostPets);
 // 17 - Write a *switch* statement that logs:
 //      "First is the worst" if your lucky number is 1
@@ -66,7 +73,20 @@ console.log(mostPets);
 //      "Third is the one with the polka dot dress" if your lucky number is 3
 //      Otherwise, log "Luck is what happens when preparation meets opportunity"
 //      You'll need to research how to use switch statements!
-
+switch (luckyNumber) {
+    case 1:
+        console.log("First is the worst");
+        break;
+    case 2:
+        console.log("Second is the best");
+        break;
+    case 3:
+        console.log("Third is the one with the polka dot dress");
+        break;
+    default:
+        console.log("Luck is what happens when preparation meets opportunity")
+};
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
 
+adventurous ? console.log("Adventures are great!") : console.log("How about we stay home?");
 
